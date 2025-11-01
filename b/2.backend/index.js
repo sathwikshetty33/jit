d64e4661f362602888c840bf07ff7c8904b9fdf1
RITVIKKAMASETTY <ritvikksymp@gmail.com>
2025-11-01 16:55:20 +0530

    Add p/jit-deomproject as a regular folder

--- b/2.backend/index.js
+++ b/2.backend/index.js
@@ -5,7 +5,6 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, this is a basic Node.js server!');
 });
-
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+server.listen(3001, () => {
+  console.log('Server running at http://localhost:3001');
 });
--- b/3.backend/index.js
+++ b/3.backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Welcome to Express Backend!');
 });
-
+ 
 app.get('/hello', (req, res) => {
   res.send('GET request received!');
 });
